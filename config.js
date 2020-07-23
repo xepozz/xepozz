770762a81ea1ffff76b836c89be6fec5606d40ba
Dmitriy Derepko <xepozz@list.ru>
2020-07-23 20:55:23 +0300

    Fix repo name

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 angular
     .module('config', [])
     .config(function ($provide) {
-        $provide.constant('REPOSITORY_NAME', 'xepozz/blogit')
+        $provide.constant('REPOSITORY_NAME', 'xepozz/xepozz')
         $provide.constant('DEBUG_ENABLED', true)
         $provide.constant('POST_REQUIRED_TAGS', ['published'])
     })
